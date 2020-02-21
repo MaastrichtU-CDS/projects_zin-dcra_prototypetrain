cd1a332efd0bc7db0f8cd9adf54b24fa3d9a6130
Johan van Soest <johan.vansoest@maastro.nl>
2020-02-21 15:46:58 +0100

    Testing with one station only

--- src/stationAlgorithm.py
+++ src/stationAlgorithm.py
@@ -1,14 +1,26 @@
 import json, logging
+import os
+from SPARQLWrapper import SPARQLWrapper, JSON
+
 def stationAlgorithm(inputStr):
     outputStr = ''
     logging.info('starting new calculation')
     # --------------------------- PASTE YOUR ALGORITHM HERE -------------------------------
     # define the "outputStr" variable
     # perform station algorithm
-    inputJson = json.loads(inputStr)
-    inputNumber = float(inputJson['number_to_process'])
-    outputNum = inputNumber * 2
-    outputStr = json.dumps({'calculation_result': outputNum})
+
+    logging.info("Endpoint URL: " + os.environ.get("sparql_url"))
+
+    sparql = SPARQLWrapper(os.environ.get("sparql_url"))
+    sparql.setQuery("""PREFIX db: <http://localhost/rdf/ontology/>
+            select (COUNT(?s) AS ?myCount) where { 
+                ?s ?p db:Tumour_Treatment.
+            }
+        """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    outputStr = json.dumps(results)
 
     logging.info('done with calculation')
 
